5fbea49b8c10402c9254d653df21d2e8a6972d3c
Admin <admin@Guests-MacBook-Pro.local>
2016-03-07 21:45:39 -0600

    updating tests

--- tests.js
+++ tests.js
@@ -1,11 +1,11 @@
 CryptoIdentity.generateCurrentIdentity().then(function(identity){
+  var safeIdentity = CryptoIdentity.getSafeIdentity(identity);
   QUnit.test( "identity should had data", function( assert ) {
     assert.ok( identity.private != null);
     assert.ok( identity.public != null);
   });
 
   QUnit.test( "should be able to get safe identity", function( assert ) {
-    var safeIdentity = CryptoIdentity.getSafeIdentity(identity);
     assert.ok( safeIdentity.private === undefined);
     assert.ok( safeIdentity.public != null);
   });
@@ -13,13 +13,13 @@ CryptoIdentity.generateCurrentIdentity().then(function(identity){
   QUnit.test( "should be able to verify messages", function( assert ) {
     var message = "Hello World!"
     var signature = CryptoIdentity.createMessageSignature(identity,message)
-    assert.ok(CryptoIdentity.verifyMessageSignature(identity,message,signature))
-    assert.ok(CryptoIdentity.verifyMessageSignature(identity,message,"")==false)
+    assert.ok(CryptoIdentity.verifyMessageSignature(safeIdentity,message,signature))
+    assert.ok(CryptoIdentity.verifyMessageSignature(safeIdentity,message,"")==false)
   });
 
   QUnit.test( "should be able to encrypt and decrypt message", function( assert ) {
     var message = "Hello World!"
-    var encryptedMessage = CryptoIdentity.encrypt(identity,message)
+    var encryptedMessage = CryptoIdentity.encrypt(safeIdentity,message)
     assert.ok(CryptoIdentity.decrypt(identity,encryptedMessage)==message);
   });
 })
